6694c97d0fb2b7f4521b22a4657c05363f9f4277
Ryan Cheung <rcheung@tesla.com>
2017-05-17 11:22:50 -0700

    Add additional express web services to demonstrate proxy-N services routing

--- src/proxy.js
+++ src/proxy.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 
-var apiProxy = proxy('/weba', {
-    target: 'http://weba:5000',
-    changeOrigin: true,   // for vhosted sites
-    pathRewrite: {
-      '^/weba': ''
-    }
-});
-
 var app = express();
 
-app.use(apiProxy);
+app.use(proxy('/web-a', {
+    target: 'http://web-a:5000',
+    changeOrigin: true,   // for vhosted sites
+    pathRewrite: {
+      '^/web-a': ''
+    }
+}));
+
+app.use(proxy('/web-b', {
+    target: 'http://web-b:5000',
+    changeOrigin: true,   // for vhosted sites
+    pathRewrite: {
+      '^/web-b': ''
+    }
+}));
+
+app.use(proxy('/web-c', {
+    target: 'http://web-c:5000',
+    changeOrigin: true,   // for vhosted sites
+    pathRewrite: {
+      '^/web-c': ''
+    }
+}));
+
 app.listen(3000);
